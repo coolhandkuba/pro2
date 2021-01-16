cbabc03050e53b081cf28a48ad5e07d958b3136b
Pendergast <coolhandkuba@hotmail.com>
2021-01-16 11:29:17 -0500

    updated all files with boilerplate

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO recipes (recipe_name, recipe) VALUES ('BBQ Bacon Burger', 'testng');
-INSERT INTO burgers (recipe_name, recipe) VALUES ('Melted Swiss and Mushroom Burger', 'testing 2');
-INSERT INTO burgers (recipe_name, recipe) VALUES ('Traditional Cheese Burger', 'testing 3');
--- server.js
+++ server.js
@@ -1,32 +1,36 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+// Requiring necessary npm packages
+const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 
-var port = process.env.PORT || 8080;
-// var port = 8080;
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
 
-var app = express();
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static(process.cwd() + "/public"));
-app.use(express.static(process.cwd() + "/public"));
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/recipe_controller.js");
-
-app.use("/", routes);
-
-app.listen(port, function() {
-    console.log(`Server running http://localhost:${port}, Ctrl + c to stop`);
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
